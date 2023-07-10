7eb251ae28299236603a9e3fb3ec256ca8374012
YevhenBez <byxgalter@bigmir.net>
2023-07-11 01:19:37 +0300

    Написал    router    для получения контакта по      ID     Модуль 2     Node.js
    Написал    router    для получения контакта по      ID    Для этого импортнул функцию    HttpError    а в   try     Роутера    получил    ID     при помощи     const { id } = req.params;   и получил искомый по   ID  контакт  при помощи функции     getContactById     с  файла        models\contacts.js    При чём если искомого по   ID  контакта не существует, то прокинул функцию    HttpError    с необходимымым, согласно ТЗ, текстом при помощи    throw HttpError(404, "Not found");  иначе возвращаю искомый по   ID  контакт при помощи  res.json(result);    Также обрати внимание, что т.к. при ошибке получается в       catch     одинаковая ошибка что в данном Роутере, что в Роутере всего массива контактов, то применил аргумент   next    , который при ошибке срабатывает в    catch    в виде   next(error)      Модуль 2     Node.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,18 +2,30 @@ const express = require('express')
 
 const contacts = require("../../models/contacts")
 
+const {HttpError} = require("../../helpers/HttpError")
+
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
     res.status(200).json(result)
   }
-  catch(error) {res.status(500).json({message: "Server error"})}
+  catch(error) {next(error)}
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
