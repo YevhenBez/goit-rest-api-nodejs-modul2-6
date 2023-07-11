151a4fbba8932254276fb2f4c05ce2786fc134b3
YevhenBez <byxgalter@bigmir.net>
2023-07-11 23:47:15 +0300

    Написал    router    для обновления данных в контакте. Модуль 2     Node.js
    Написал    router    для обновления данных в контакте. Для этого при помощи метода  validate    получил ошибку (если таковая будет). Дальше идёт проверка - если ошибка есть, то при помощи функции    HttpError    прокидываю статус 400    саму ошибку   при помощи   error.message    и дальше произвольный текст. Также получил   id    искомого контакта    при помощи   const { id } = req.params;     А в переменной     result    получил результат обновления данных в контакте при помощи вызова функции   updateContact   с файла   contacts.js   который находится в папке    models     Причём данная функция получает   id    и тело запроса (значения, которые пользователь хочет перезаписать)    req.body    Далее обычная проверка   - если результата нет, то при помощи    функции   HttpError   прокидываю статус    404    и текст (пиши что хочешь)    Not found     иначе возвращаю результат обновления данных контакта со статусом   200. Отлавливание ошибок в    catch   аналогично алгоритмам описанным в предыдущих комитах.  В файле    contacts.json    просто отобразилось, что я перезаписал данные контакта. Чтобы проверить данный алгоритм в  приложухе   Postman   необходимо    зайти в    My Workspace    выбрать    PUT    прописать путь в строке     Postman    в данном случае это     http://localhost:3000/api/contacts/rsKkOQUi80UsgVPCcLZZW      где  крайнее звено   -   это   id     контакта в котором мы будем перезаписывать значения. В разделе ниже   выбираешь    Body    включаешь флаг   raw   и выбираешь JSON    Данные, которые ты хочешь изменить в данном контакте, прописываешь в формате     {
        "name": "Alec Howard Proverka 11.07.2023 v 22.27.01",
        "email": "prov@000001.net",
        "phone": "111-11-11"
    }    и нажимаешь кнопку    Send      Модуль 2     Node.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -63,8 +63,22 @@ router.delete('/:id', async (req, res, next) => {
   }
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, `${error.message} : missing fields`);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json(result);
+  }
+   catch (error) {
+    next(error);
+  }
 })
 
 module.exports = router
