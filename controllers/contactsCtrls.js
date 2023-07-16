9f6c11d56c7c69e5fc4ee28d7c446064766b19f3
YevhenBez <byxgalter@bigmir.net>
2023-07-16 17:42:39 +0300

    Теперь переделываю код делая ту же самую логику, что и была, но уже при помощи функции обёртки (цель - ещё большее сокращение кода). Часть Первая - перекалбашиваю логику получения всех контактов. Модуль 3 Node.js
    Теперь переделываю код делая ту же самую логику, что и была, но уже при помощи функции обёртки (цель - ещё большее сокращение кода). Часть Первая - перекалбашиваю логику получения всех контактов. Для этого в файле    ctrlWrapper.js     создаю функцию-обёртку    и сразу её экспортирую при помощи     module.exports    Затем в файле   contactsCtrls.js    импортирую  данную функцию обёртку    ctrlWrapper     а в функции   listContactsCtrls    убираю    try { } catch (error) {
        next(error);
      }    и для экспорта функции    listContactsCtrls    применяю такой код    listContactsCtrls: ctrlWrapper(listContactsCtrls),   Модуль 3 Node.js

--- controllers/contactsCtrls.js
+++ controllers/contactsCtrls.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const contacts = require("../models/contacts");
 
 const { HttpError } = require("../helpers/HttpError");
+const { ctrlWrapper } = require("../helpers/ctrlWrapper");
 
 const addSchema = Joi.object({
   name: Joi.string().required(),
@@ -11,12 +12,9 @@ const addSchema = Joi.object({
 });
 
 const listContactsCtrls = async (req, res, next) => {
-  try {
     const result = await contacts.listContacts();
     res.status(200).json(result);
-  } catch (error) {
-    next(error);
-  }
+  
 }
 
 const getContactByIdCtrls = async (req, res, next) => {
@@ -76,7 +74,7 @@ const updateContactCtrls = async (req, res, next) => {
 }
 
 module.exports = {
-    listContactsCtrls,
+    listContactsCtrls: ctrlWrapper(listContactsCtrls),
     getContactByIdCtrls,
     addContactCtrls,
     removeContactCtrls,
