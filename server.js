9bf19698aebcaace968bab5166a00682ae008f1c
YevhenBez <byxgalter@bigmir.net>
2023-07-19 15:57:58 +0300

    Все изменения в данном файле - это форматирование при помощи Prittier    Модуль 3     Node.js
    Все изменения в данном файле - это форматирование при помощи Prittier    Модуль 3     Node.js

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
 
-const DB_HOST = "mongodb+srv://byxgalter:v4J6ko42TsqaV6DE@cluster0.4p2s1zr.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+const DB_HOST =
+  "mongodb+srv://byxgalter:v4J6ko42TsqaV6DE@cluster0.4p2s1zr.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST)
-  .then(() => {app.listen(3000, () => {
-  console.log("Database connection successful")
-})
-})
-  .catch(error => {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
     console.log(error.message);
     process.exit(1);
-  })
-
-
+  });
