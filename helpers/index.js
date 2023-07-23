4294c5d0a078939569cab27ee0c3f03a3ec5ffb0
YevhenBez <byxgalter@bigmir.net>
2023-07-23 14:03:17 +0300

    Сделал реэкспорт    функции   handleMongooseError     Модуль 4    Node.js
    Сделал реэкспорт    функции   handleMongooseError     Для этого в файле     index.js     (который находится в папке    helpers    )     импортнул     функцию   handleMongooseError     и сразу же экспортнул её. В связи с данными манипуляциями уже не было необходимости в файле   contacts.js     (который находится в папке    models      ) указывать полный путь для импорта    функции   handleMongooseError     Достаточно указать папку ( в которой находится файл с  функцией   handleMongooseError      )   Модуль 4    Node.js

--- helpers/index.js
+++ helpers/index.js
@@ -2,7 +2,10 @@ const { HttpError } = require("../helpers/HttpError");
 
 const ctrlWrapper = require("../helpers/ctrlWrapper");
 
+const handleMongooseError = require("../helpers/handleMongooseError");
+
 module.exports = {
     HttpError,
-    ctrlWrapper
+    ctrlWrapper,
+    handleMongooseError
 };
