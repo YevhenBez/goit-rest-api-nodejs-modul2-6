74e495b18a26d443f6aa74c3d38bcd4e1f343712
YevhenBez <byxgalter@bigmir.net>
2023-07-26 15:18:55 +0300

    Реализовал пагинацию. Модуль 4    Node.js
    Реализовал пагинацию. Для этого с объекта поиска   req.query     через деструктуризацию получаем страницу       page     и количество записей на странице      limit    и сразу, по умолчанию, присвоил им значение 1 и 20 соответственно. Обращаю внимание, что значения объекта поиска указываются после вопросительного знака в строке браузера.  Затем в переменной    skip    прописал формулу для расчёта данного значения. Значение    skip   даёт нам возможность, при необходимости, дать указание, сколько записей нужно пропустить и с какой записи начинать показывать.    Затем, в связи с дописанием выше описанной логики, немного изменил код переменной    result     а именно, второе значение у метода      find    это какие данные нужно убрать (или какие только выводить) из выводимого, в данном случае, контакта. Но т.к. это нам не нужно, то я указал     ""     а вот третий аргумент данного метода    find     это описанные выше    skip   и  limit    Для того, чтобы проверить пагинацию в    Postman    нужно зайти в     My Workspace    затем выбрать    GET      и прописать адрес        http://localhost:3000/api/contacts?page=1&limit=20 Затем выбери закладку     Authorization    а в  Type   укажи   Bearer Token     Затем в поле  Token   укажи  правильный Токен (который ты можешь взять с ответа в    Postman   полученного при залогинивании пользователя). Далее на закладке     Params     ты можешь указывать с какой страницы    (     page    ) ты хочешь смотреть записи и сколько записей    (    limit    ) должно быть на одной странице. Затем нажимаешь кнопку    Send     Модуль 4    Node.js

--- controllers/listContactsCtrls.js
+++ controllers/listContactsCtrls.js
@@ -2,7 +2,9 @@ const { ContactMongoose } = require("../models");
 
 const listContactsCtrls = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await ContactMongoose.find({owner});
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await ContactMongoose.find({owner}, "", {skip, limit});
   res.status(200).json(result);
 };
 
