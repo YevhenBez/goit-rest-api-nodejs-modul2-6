9f635308de230bab7e3ea981f35058e8384e15f2
YevhenBez <byxgalter@bigmir.net>
2023-08-06 13:45:18 +0300

    В схеме пользователя добавил поля для проверки верифицировал ли он мыло при регистрации и для кода подтверждения мыла. Модуль 6    Node.js
    В схеме пользователя, которая сделана на базе библиотеки    mongoose     добавил поля для проверки верифицировал ли данный пользователь мыло при регистрации       (это поле   verify    )  и для кода подтверждения мыла (это поле    verificationCode    ). Модуль 6    Node.js

--- models/user.js
+++ models/user.js
@@ -36,6 +36,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      default: ""
+    }
   },
   { versionKey: false }
 );
